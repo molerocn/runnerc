eac2857d13712ffda29a7886c76758f430c51899
Juan Carlos <carlos.molero.n@gmail.com>
2025-09-12 03:47:35 -0500

    create transitions between animations (prototypes only for flying and swimming)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,23 +4,34 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public float jump;
+    public Animator animator;
 
     private Rigidbody2D rb;
     public bool isJumping;
+    private bool isInLobby = true;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        rb.linearVelocity = new Vector2(speed, rb.linearVelocityY);
+        if (!isInLobby)
+        {
+            rb.linearVelocity = new Vector2(speed, rb.linearVelocityY);
+        }
         if (Input.GetButtonDown("Jump") && !isJumping)
         {
-            rb.AddForce(new Vector2(rb.linearVelocityX, jump));
+            if (isInLobby)
+            {
+                isInLobby = false;
+                animator.SetBool("isInLobby", false);
+            }
+            else
+            {
+                rb.AddForce(new Vector2(rb.linearVelocityX, jump));
+            }
         }
     }
 
@@ -29,6 +40,7 @@ public class PlayerMovement : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isJumping = false;
+            animator.SetBool("isJumping", false);
         }
     }
     private void OnCollisionExit2D(Collision2D collision)
@@ -36,6 +48,7 @@ public class PlayerMovement : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isJumping = true;
+            animator.SetBool("isJumping", true);
         }
     }
 }
